b7683a3e7f40c96c0e9c987a98cf8edeb3933d64
magnun27 <108046361+magnun27@users.noreply.github.com>
2022-07-22 09:56:50 -0400

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'valeskavalenzuela10@gmail.com';
+$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+
+mail($para, $asunto, utf8_decode($message), $header);
+echo "<script>alert ('correo enviado') </script>";
+echo "<script> setTimeout (\"Location.href='index.html'\",1000</script>";
+?>
